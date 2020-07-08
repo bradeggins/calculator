2d560b4729cab64d81e49c560eca5c40a047c785
Bradley Eggins <brad.eggins@gmail.com>
2020-07-08 21:52:27 +1200

    Add function to calc numbers more than one digit

--- calculator.js
+++ calculator.js
@@ -1,75 +1,78 @@
-// Declare global variables for elements
-var valuesToCalculate = []
-var total = 0
-var keyedValue = ""
-// Listen
-    // Add event listener to get button value
-    var buttonPress = document.addEventListener('click', calculate)
-// Get button value
-    // From event listener
-    function calculate(){
-        var pressedValue = event.target.value;
-        // If AC then call allClear
-        if(pressedValue === "AC"){
-            allClear();
-            displayTotal(total)
-        // If CE then call clear
-        } else if (pressedValue === "CE"){
-            clearEntry();
-            displayTotal(total)
-        // If = then replace display with running total 
-        } else if (pressedValue === "="){
-            calculateTotal()
-            displayTotal(total)
-        } else if (pressedValue != NaN){
-            valuesToCalculate.push(pressedValue)
-            displayTotal(pressedValue)
-        } 
-    
-    }
-    
-    
-    // If = then replace display with running total
-    // If */+- then calculate 
-    // Else store number
+// Array to store calculations 
+var valuesToCalculate = [];
+// Set total to zero.
+var total = 0;
+// Variable for storing a number more than one digit long
+var keyedValue = "";
+// Add event listener to get button value
+var buttonPress = document.addEventListener('click', calculate);
 
+
+function calculate(){
+    // Get presed value from html and evnet listener
+    var pressedValue = event.target.value;
+    // If AC then call allClear
+    if(pressedValue === "AC"){
+        allClear();
+        displayTotal(total);
+    // If CE then call clear
+    } else if (pressedValue === "CE"){
+        clearEntry();
+        displayTotal(total);
+    // If = then replace display with running total 
+    } else if (pressedValue === "="){
+        // Push number string to array
+        valuesToCalculate.push(keyedValue);
+        // Reset storage for number string to 0
+        keyedValue = "";
+        calculateTotal();
+        displayTotal(total);
+    // Check if operation sign
+    } else if (pressedValue === "+" || pressedValue === "-" || pressedValue === "*" || pressedValue === "/") {
+        // Push number string to array
+        valuesToCalculate.push(keyedValue);
+        // Reset storage for number string to 0
+        keyedValue = ""
+        // Add operator to array
+        valuesToCalculate.push(pressedValue);
+        displayTotal(pressedValue);
+    }
+        else if (pressedValue != NaN){
+        // Add more than one digit to storage
+        keyedValue = keyedValue.concat(pressedValue);
+        displayTotal(keyedValue);
+    } 
+
+}
+    
 // Clear everything and reset calc 
 function allClear(){
     //Erase current stored number
-    console.log("AC")
-    keyedValue = ""
+    console.log("AC");
+    keyedValue = "";
     // Erase global array
-    valuesToCalculate = []
+    valuesToCalculate = [];
     //Set values to zero
-    total = 0
+    total = 0;
 }
 // Clear last inputed value
 function clearEntry(){
-    console.log("CE")
+    console.log("CE");
     // Erase current stored number
-    keyedValue = ""  
+    keyedValue = ""; 
     // Set display to 0
-    total = 0
+    total = 0;
 }
-    
-
-    
-// Store number
-    // If button press is a number then store in temp string
-    // When another button pressed +-*/ store in array
-    
+      
 // Calculate total    
 function calculateTotal(){
-    console.log(valuesToCalculate)
     var calcString = valuesToCalculate.join(' ');
-    console.log(calcString)
-    total = eval(calcString)
-    console.log(total)
+    total = eval(calcString);
 }
 
 // Display total on LCD screen
 function displayTotal(inputed){
-    document.getElementById("display").value = inputed
+    document.getElementById("display").value = inputed;
 }
 
 
